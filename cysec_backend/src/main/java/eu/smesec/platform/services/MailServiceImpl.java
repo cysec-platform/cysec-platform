abb59f3633edd00686d8abf7bcf9ecf053ecf16b
Matthias Luppi <53262016+mluppi@users.noreply.github.com>
2021-04-13 08:52:33 +0200

    Make sender address configurable

--- cysec_backend/src/main/java/eu/smesec/platform/services/MailServiceImpl.java
+++ cysec_backend/src/main/java/eu/smesec/platform/services/MailServiceImpl.java
@@ -14,6 +14,9 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
+import eu.smesec.platform.config.Config;
+import eu.smesec.platform.config.CysecConfig;
 import org.glassfish.jersey.logging.LoggingFeature;
 
 /**
@@ -24,18 +27,28 @@ import org.glassfish.jersey.logging.LoggingFeature;
 public class MailServiceImpl implements MailService {
   private static final Logger logger = Logger.getLogger(LoggingFeature.DEFAULT_LOGGER_NAME);
 
-  private static final String MAIL_SENDER = "SMESEC-Framework-platform";
+  private static final String CONFIG_MAIL_SENDER_ADDRESS = "cysec_mail_sender_address";
+  private static final String DEFAULT_MAIL_SENDER_ADDRESS = "no-reply@example.com";
 
   /**
    * Sends an email to the local SMTP server.
    * @param recipient The user to receive the email.
-   * @param cc A Carbon Copy receiver.
-   * @param bcc A Blind Copy receiver.
+   * @param cc A carbon copy receiver.
+   * @param bcc A blind carbon copy receiver.
    * @param subject The email subject line.
    * @param body The content of the email.
    */
   @Override
   public void sendMail(User recipient, String cc, String bcc, String subject, String body) {
+    final Config config = CysecConfig.getDefault();
+    final String mailSender = config.getStringValue(null, CONFIG_MAIL_SENDER_ADDRESS);
+
+    // skip sending mail when no sender address is configured
+    if (DEFAULT_MAIL_SENDER_ADDRESS.equals(mailSender)) {
+      logger.warning("No configuration for '" + CONFIG_MAIL_SENDER_ADDRESS + "' found");
+      return;
+    }
+
     if (recipient == null || recipient.getEmail() == null || recipient.getEmail().equals("")) {
       throw new IllegalArgumentException();
     }
@@ -49,7 +62,7 @@ public class MailServiceImpl implements MailService {
 
     try {
       final MimeMessage msg = new MimeMessage(session);
-      msg.setFrom(new InternetAddress(MAIL_SENDER));
+      msg.setFrom(new InternetAddress(mailSender));
       msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient.getEmail(), false));
       if (cc != null && !cc.isEmpty()) {
         msg.setRecipients(Message.RecipientType.CC, InternetAddress.parse(cc, false));
@@ -68,8 +81,8 @@ public class MailServiceImpl implements MailService {
   /**
    * Method overloading for sendMail, allows to send email to multiple receivers at once.
    * @param recipients A list of users who will receive the email.
-   * @param cc A Carbon Copy receiver.
-   * @param bcc A Blind Copy receiver.
+   * @param cc A carbon copy receiver.
+   * @param bcc A blind carbon copy receiver.
    * @param subject The email subject line.
    * @param body The content of the email.
    */
