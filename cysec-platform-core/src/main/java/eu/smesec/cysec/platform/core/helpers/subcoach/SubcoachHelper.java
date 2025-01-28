32399b6cfce67115f27b9e81eb4992540659bc6c
Aaron Ebnöther <aaron.ebnoether@fhnw.ch>
2025-01-28 11:05:57 +0100

    Fix Bug caused by QuestionType String -> Enum change

--- cysec-platform-core/src/main/java/eu/smesec/cysec/platform/core/helpers/subcoach/SubcoachHelper.java
+++ cysec-platform-core/src/main/java/eu/smesec/cysec/platform/core/helpers/subcoach/SubcoachHelper.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -102,7 +102,7 @@ public class SubcoachHelper {
                     .getQuestions()
                     .getQuestion()
                     .stream()
-                    .filter(q -> q.getType().equals("subcoachInstantiatorOutlet"))
+                    .filter(q -> q.getType().equals(QuestionType.SUBCOACH_INSTANTIATOR_OUTLET))
                     .filter(q -> q.getSubcoachInstantiatorId().equals(instantiatorId.get()))
                     .map(Question::getId)
                     .findFirst();
